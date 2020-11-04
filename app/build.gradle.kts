94ce03d5c1cae5366e44effb016572c60a2e6512
Thomas Wirth <thomas-w-@web.de>
2020-11-04 21:54:21 +0100

    Enable multidex for comparability

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,13 +13,11 @@ android {
         targetSdkVersion(30)
         versionCode = 1
         versionName = "1.0"
+        applicationIdSuffix = ".joda"
+        multiDexEnabled = true
     }
     buildTypes {
-        getByName("debug") {
-            applicationIdSuffix = ".joda"
-        }
         getByName("release") {
-            applicationIdSuffix = ".joda"
             isShrinkResources = true
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
