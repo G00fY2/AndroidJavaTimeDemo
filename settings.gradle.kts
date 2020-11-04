2e196e4cfd9f16894cae4f96f626a2f98da1b2b8
Thomas Wirth <thomas-w-@web.de>
2020-11-04 21:45:50 +0100

    Add dexcount plugin
    (cherry picked from commit 4c227b80fc53ed754f16e6e5cd64d686ce3c7d8d)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,17 +3,17 @@ include("app")
 pluginManagement {
     val kotlinVersion: String by settings
     val androidGradlePluginVersion: String by settings
+    val dexcountPluginVersion: String by settings
     repositories {
         google()
         gradlePluginPortal()
     }
     resolutionStrategy {
         eachPlugin {
-            if (requested.id.id == "com.android.application") {
-                useModule("com.android.tools.build:gradle:$androidGradlePluginVersion")
-            }
-            if (requested.id.namespace == "org.jetbrains.kotlin") {
-                useVersion(kotlinVersion)
+            when {
+                requested.id.id == "com.android.application" -> useModule("com.android.tools.build:gradle:$androidGradlePluginVersion")
+                requested.id.id == "com.getkeepsafe.dexcount" -> useVersion(dexcountPluginVersion)
+                requested.id.namespace == "org.jetbrains.kotlin" ->  useVersion(kotlinVersion)
             }
         }
     }
