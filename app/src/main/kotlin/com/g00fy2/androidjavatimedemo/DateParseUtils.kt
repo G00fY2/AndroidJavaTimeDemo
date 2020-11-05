03a87eaeb3241fa4c0a28cbc0521f00444be6489
Thomas Wirth <thomas-w-@web.de>
2020-11-05 16:46:46 +0100

    Convert date to local time zone when parsing

--- app/src/main/kotlin/com/g00fy2/androidjavatimedemo/DateParseUtils.kt
+++ app/src/main/kotlin/com/g00fy2/androidjavatimedemo/DateParseUtils.kt
@@ -2,6 +2,8 @@ package com.g00fy2.androidjavatimedemo
 
 import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalDateTime
+import org.threeten.bp.OffsetDateTime
+import org.threeten.bp.ZoneId
 import org.threeten.bp.format.DateTimeFormatter
 
 object DateParseUtils {
@@ -12,6 +14,7 @@ object DateParseUtils {
             dateStrings.forEach {
                 // size check is not safe to use
                 val date = when {
+                    it.length >= 24 -> OffsetDateTime.parse(it, DateTimeFormatter.ISO_DATE_TIME).atZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime()
                     it.length >= 18 -> LocalDateTime.parse(it, DateTimeFormatter.ISO_DATE_TIME)
                     else -> LocalDate.parse(it, DateTimeFormatter.ISO_DATE).atStartOfDay()
                 }
